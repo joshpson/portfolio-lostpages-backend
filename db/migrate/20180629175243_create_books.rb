8b2a460057fe17dc9556ecea361e25c8caa22899
joshpson <jpearson846@gmail.com>
2018-07-03 17:10:11 -0400

    book schema changes

--- db/migrate/20180629175243_create_books.rb
+++ db/migrate/20180629175243_create_books.rb
@@ -1,11 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-      t.string "name"
+      t.string "title"
       t.string "author"
-      t.string "short_description"
-      t.string "info_link"
-      t.string "cover_img"
+      t.string "image"
       t.integer "user_id"
       t.integer "condition"
       t.string "notes"
